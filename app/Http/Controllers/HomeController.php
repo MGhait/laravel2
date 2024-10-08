cda8fa6d626160b3d1335b87a5751d40aabea0a3
Mohamed Saad <mosaad05019@gmail.com>
2024-10-08 18:31:56 +0300

    interact with request

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,6 +18,62 @@ class HomeController extends Controller
         // dd(Route::currentRouteName());
         // dd($request->route()->getAction());
         // dd(Route::currentRouteAction());
-        return view('welcome');
+
+        // Request Path & Methods
+        // dd($request->path());
+        // dd($request->is('/'));
+        // dd($request->routeIs('Gheit route')); // flase ==> [Gheit Route]
+        // dd($request->url());  //  main url only
+        // dd($request->fullUrl());  // main url + all qurey paramter
+        // dd($request->fullUrlWithoutQuery('age'));
+        // dd($request->fullUrlWithoutQuery(['age', 'name']));
+        // dd($request->fullUrlWithoutQuery('age'));
+        // dd($request->method());
+        // dd($request->isMethod('get'));
+        
+        
+        // Request Inputs
+        // dd($request->all());  // return array
+        // dd($request->collect());  // return collection
+        // $request->collect()->each(function ($elment) {
+        //     dump($elment);
+        // });  
+        
+        // dd($request->input());  // same as all -> gets the input form elment
+        // dd($request->query());  // same as all -> gets the qurey form url
+        // dd($request->boolean('is_admin'));  
+        // dd($request->date('data'));   // return carbon collection
+        // dd($request->only(['name', 'age']));
+        // dd($request->except(['name', 'age']));
+        
+        
+        //Request Inputs Presence  [has, filled, missing]
+
+        // dd($request->has('age')); 
+        // dd($request->hasAny(['name', 'age'])); 
+        // dd($request->whenHas('age', function(){
+        //     dd('request has age');
+        // }, function (){
+        //     dd('request dose not have age');
+        // })); 
+
+        // dd($request->filled('age')); 
+        // dd($request->anyFilled(['name', 'age'])); 
+        // dd($request->whenFilled('age', function(){
+        //     dd('request has filled age');
+        // }, function (){
+        //     dd('request dose not have filled age');
+        // })); 
+
+        // dd($request->missing('value'));  // opposite of has 
+        // dd($request->whenMissing('age', function(){
+        //     dd('age is missing');
+        // }, function (){
+        //     dd('age exists');
+        // })); 
+
+
+
+        // return view('welcome');
     }
 }
