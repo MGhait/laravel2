cf1cb4eef70f95d6e0af33ca52278b46915e0350
Mohamed Saad <mosaad05019@gmail.com>
2024-10-10 06:55:46 +0300

    errorspages

---
+++ resources/views/errors/400.blade.php
@@ -0,0 +1,5 @@
+@extends('errors::minimal')
+
+@section('title', "Bad Requist")
+@section('code', '400')
+@section('message', "Bad Requist")
---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,7 @@
+{{-- @extends('errors::minimal') --}}
+@extends('errors::layout')
+
+{{-- @section('title', __('Forbidden')) --}}
+@section('title', "custom title")
+{{-- @section('code', '403') --}}
+@section('message', '403 custom forbidden')
